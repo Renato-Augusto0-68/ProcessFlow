2094a718f8fb2eccb135accc71652737a06c722c
Renato-Augusto0-68 <rass@cesar.school>
2026-08-17 14:50:09 -0300

    main basic structuration

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+typedef struct task{
+    char nome [60];
+    char programa[60];
+
+
+}task;
+
+
+int main(){
+    
+
+
+}
